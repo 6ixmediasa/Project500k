93a5e28323e1b78c52fefe22bcbf949e355e59ac
6ixmediasa <6ixmediasa@gmail.com>
2022-07-21 06:17:25 +0200

    commit

---
+++ common/config.php
@@ -0,0 +1,31 @@
+<?php
+
+define('OK_LOADME', 1);
+error_reporting(E_ALL ^ E_NOTICE ^ E_STRICT);
+
+// --- start ---
+
+date_default_timezone_set('UTC');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'cHJvamVjdGtfMQ==');
+define('DB_PASSWORD', 'WW91ckxvdmU1ODUwNQ==');
+define('DB_NAME', 'projectk_1');
+define('DB_TBLPREFIX', 'netw');
+
+define('INSTALL_PATH', '/home/projectk/public_html');
+define('DECIMAL_POINT', 2);
+
+define('DEFIMG_LOGO', '../assets/image/logo_defaultimage.png');
+define('DEFIMG_PLAN', '../assets/image/plan_defaultimage.jpg');
+define('DEFIMG_FILE', '../assets/image/file_defaultimage.jpg');
+define('DEFIMG_SITE', '../assets/image/site_defaultimage.jpg');
+define('DEFIMG_ADM', '../assets/image/adm_defaultimage.jpg');
+define('DEFIMG_MBR', '../assets/image/mbr_defaultimage.jpg');
+
+define('ADMFOLDER_NAME', 'admin');
+define('MBRFOLDER_NAME', 'member');
+define('UIDFOLDER_NAME', 'id');
+
+//ini_set('log_errors', 'On');
+//ini_set('error_log', 'netw_error.log');
